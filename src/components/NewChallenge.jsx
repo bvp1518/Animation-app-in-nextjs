e179df7d18ef95301ef9c1c17dfe7dbb7ebfed70
bvp1518 <bvp18061992@gmail.com>
2024-09-30 12:09:27 +0530

    till-finaly done

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -33,22 +33,22 @@ export default function NewChallenge({ onDone }) {
       !challenge.description.trim() ||
       !challenge.deadline.trim() ||
       !challenge.image
-    ) {
+    )
+     {
       animate(
         'input,textarea',
-        {x: [-10, 0, 10, 0]}, 
+        {x: [-10, 0, 10, 0] }, 
         {type: 'spring', duration:0.2, delay:stagger(0.05)}
       );
       return;
     }
-
     onDone();
     addChallenge(challenge);
   }
 
   return (
     <Modal title="New Challenge" onClose={onDone}>
-      <form id="new-challenge" onSubmit={handleSubmit} ref={scope} >
+      <form id="new-challenge" onSubmit={handleSubmit} ref={scope}>
         <p>
           <label htmlFor="title">Title</label>
           <input ref={title} type="text" name="title" id="title" />
@@ -64,15 +64,17 @@ export default function NewChallenge({ onDone }) {
           <input ref={deadline} type="date" name="deadline" id="deadline" />
         </p>
 
-        <motion.ul id="new-challenge-images"
+        <motion.ul 
+          id="new-challenge-images"
           variants={{
-            visible: {transition: {staggerChildren: 0.05} }
-          }}>
+            visible: { transition: { staggerChildren: 0.05} },
+          }}
+          >
           {images.map((image) => (
             <motion.li
               variants={{
                 hidden: { opacity: 0, scale: 0.5 },
-                visible: { opacity: 1, scale: [0.8, 1.3, 1] }
+                visible: { opacity: 1, scale: [0.8, 1.3, 1] },
               }}
               exit={{ opacity: 1, scale: 1 }}
               transition={{type: 'spring'}}
